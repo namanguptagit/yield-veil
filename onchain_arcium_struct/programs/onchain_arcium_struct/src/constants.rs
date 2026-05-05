a49f6f405762a54ac96ec63fca6dcab091cffc8f
adityagupta0251 <itsokkadi0251@gmail.com>
2026-05-05 12:44:21 +0000

    cleaning up workspace

--- onchain_arcium_struct/programs/onchain_arcium_struct/src/constants.rs
+++
@@ -1,9 +0,0 @@
-// Seed constants for PDAs
-pub const VAULT_SEED: &[u8] = b"vault";
-pub const STRATEGY_SEED: &[u8] = b"strategy";
-pub const USER_POSITION_SEED: &[u8] = b"user_position";
-pub const ORDERBOOK_SEED: &[u8] = b"orderbook";
-pub const ORDER_SEED: &[u8] = b"order";
-pub const ESCROW_SEED: &[u8] = b"escrow";
-pub const ESCROW_AUTH_SEED: &[u8] = b"escrow_auth";
-pub const SIGN_PDA_SEED: &[u8] = b"sign_pda";
--- onchain_arcium_struct/programs/onchain_arcium_struct/src/state/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod vault;
-pub mod orderbook;
- 
